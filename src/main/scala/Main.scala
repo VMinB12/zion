a0936882ca7c045c360e3a4c3b48078dc98960a6
VMinB12 <vincent.min@b12-consulting.com>
2024-10-13 21:13:39 +0200

    spawn actors on the fly. Maybe not a good idea though?

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,39 +8,38 @@ import scala.util.Random
 
 object Agent:
   sealed trait AgentCommand
-  final case class UserTask(task: String, replyTo: ActorRef[User.Done]) extends AgentCommand
-  final case class ToolResult(result: Int, replyTo: ActorRef[User.Done]) extends AgentCommand
-
-  final case class AgentResponse(result: String)
+  final case class UserTask(task: String) extends AgentCommand
+  final case class ToolResult(result: Int) extends AgentCommand
 
   def apply(): Behavior[AgentCommand] =
     Behaviors.setup { context =>
       val tool = context.spawn(Tool(), "tool")
+      val user = context.spawn(User(), "user")
 
       Behaviors.receiveMessage {
         case message: UserTask =>
           context.log.info("Delegating task to Tool")
-          tool ! Tool.ExecuteTool(message.task, context.self, message.replyTo)
+          tool ! Tool.ExecuteTool(message.task)
           Behaviors.same
         case message: ToolResult =>
           context.log.info(s"Received ToolResult with result: ${message.result}")
-          message.replyTo ! User.Done(s"Direct response to task: ${message.result}")
+          user ! User.Done(s"Direct response to task: ${message.result}")
           Behaviors.same
       }
     }
 
 object Tool:
-  final case class ExecuteTool(
-      task: String,
-      replyTo: ActorRef[Agent.ToolResult],
-      user: ActorRef[User.Done])
+  final case class ExecuteTool(task: String)
 
   def apply(): Behavior[ExecuteTool] =
-    Behaviors.receive { (context, message) =>
-      val result = Random.nextInt(100) // Generate a random integer result
-      context.log.info(s"Tool processing task: ${message.task} with result: $result")
-      message.replyTo ! Agent.ToolResult(result, message.user)
-      Behaviors.same
+    Behaviors.setup { context =>
+      val agent = context.spawn(Agent(), "agent")
+      Behaviors.receiveMessage { message =>
+        val result = Random.nextInt(100) // Generate a random integer result
+        context.log.info(s"Tool processing task: ${message.task} with result: $result")
+        agent ! Agent.ToolResult(result)
+        Behaviors.same
+      }
     }
 
 object User:
@@ -55,7 +54,7 @@ object User:
 
       Behaviors.receiveMessage {
         case message: Start =>
-          agent ! Agent.UserTask(message.content, context.self)
+          agent ! Agent.UserTask(message.content)
           Behaviors.same
         case message: Done =>
           context.log.info(s"Done! ${message.content}")
@@ -64,6 +63,6 @@ object User:
     }
 
 object AgentQuickstart extends App:
-  val user: ActorSystem[User.UserCommand] = ActorSystem(User(), "AgentQuickstart")
+  val system: ActorSystem[User.UserCommand] = ActorSystem(User(), "AgentQuickstart")
 
-  user ! User.Start("Give me back a random integer.")
+  system ! User.Start("Give me back a random integer.")
